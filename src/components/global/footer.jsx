442c6485b59729e8fabc6ef58af1cc0db2299f30
FORMER 03 Jonas Buggisch <jonas.buggisch@gmail.com>
2023-10-22 14:59:34 +0200

    feat: footer implemented

--- src/components/global/footer.jsx
+++ src/components/global/footer.jsx
@@ -1,13 +1,15 @@
+import styles from './footer.module.scss'
+
 const Footer = () => {
   return (
-    <footer>
-      <div className='divider' />
-      <div className='content'>
-        <div className='line'>
+    <footer className={styles.footer}>
+      <div className={styles.divider} />
+      <div className={styles.content}>
+        <div className={styles.line}>
           <p>Copyright © 2023 Jonas Buggisch</p>
           <p>Contact:</p>
         </div>
-        <div className='line'>
+        <div className={styles.line}>
           <p>All rights reserved.</p>
           <p>jonas.buggisch@gmail.com</p>
         </div>
