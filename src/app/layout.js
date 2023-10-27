cb27689fcbe45602f913b25293712a0f99cc8aff
FORMER 03 Jonas Buggisch <jonas.buggisch@gmail.com>
2023-10-27 13:37:04 +0200

    feat: added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import '@/styling/style.scss'
 import Header from '@/components/global/header'
 import Footer from '@/components/global/footer'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,13 @@ export const metadata = {
 const RootLayout = ({ children }) => (
   <html lang='en'>
     <body className={inter.className}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/images/favicon/site.webmanifest" />
+      </Head>
       <Header />
       {children}
       <Footer />
