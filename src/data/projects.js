5ff7b6e66498b9ddcffb3ce01ffba75f4566be21
FORMER 03 Jonas Buggisch <jonas.buggisch@gmail.com>
2023-11-06 12:11:15 +0100

    feat: projects added

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,32 +1,22 @@
 export const projectsData = [
   {
     name: 'Sandbox-Broker',
-    shortDescription: 'Fair Trade is a Sandbox Broker that provides a safe and risk-free environment for beginners to gain experience in the stock and crypto markets.\nThe goal of this project was to create a platform that allows users to invest and experiment with assets using virtual currency, without any real financial risks.',
-    heroImageName: 'sandboxBroker/sandboxBrokerPortfolio2.png',
-    goal: '',
-    targetGroup: '',
-    features: '',
-    summary: '',
-    images: [
-      {
-        url: '',
-        altText: ''
-      }
-    ]
+    shortDescription: 'Sandbox-Broker is an app designed for novice investors to learn the basics of trading in the stock and crypto markets. It provides users with a comprehensive list of assets to choose from, along with real-time data and an easy-to-use interface. \n\nThe vision of Sandbox-Broker is to eliminate users fear of losses when investing by creating a secure and enjoyable environment where they can freely experiment without any consequences.',
+    heroImageName: 'sandboxBroker/sandboxBrokerPortfolio2.png'
+  },
+  {
+    name: 'Motivity',
+    shortDescription: 'Motivity is a weightlifting tracking app designed for the serious fitness enthusiast. Say goodbye to basic workout apps and hello to a motivating experience that focuses on your long-term progress. Motivity is not for beginners and skips the basics, providing you with a fast and convenient platform to track your weightlifting journey. \n\nWhat sets Motivity apart is its  focus on your long-term fitness objectives. You`ll find no distractions here – just the tools you need to track your lifts, set personal records, and analyze your progress over time.',
+    heroImageName: 'motivity/motivityWorkout5.png'
+  },
+  {
+    name: 'Banaenae',
+    shortDescription: 'I`m excited to share Banaenae, a 2D local multiplayer platformer game that my team and I developed during the 8th MMarmelade Game Jam. As a group of web developers, we took on the challenge of creating a game without any prior experience in the field. \n\nThe game features a retro-style pixel art graphics, where the player controls a small banana character and competes with an opponent to reach the top of the level faster.',
+    heroImageName: 'banaenae/banaenaeGameplay2.png'
   },
   {
     name: 'Time is Precious',
-    shortDescription: 'Fair Trade is a Sandbox Broker that provides a safe and risk-free environment for beginners to gain experience in the stock and crypto markets.\nThe goal of this project was to create a platform that allows users to invest and experiment with assets using virtual currency, without any real financial risks.',
-    heroImageName: 'timeIsPrecious/timeIsPreciousDashboard3.png',
-    goal: '',
-    targetGroup: '',
-    features: '',
-    summary: '',
-    images: [
-      {
-        url: '',
-        altText: ''
-      }
-    ]
+    shortDescription: 'Time is Precious is a project-based time management and tracking tool designed for freelance and self-employed professionals in the tech and creative industries. \n\nWe simplify the process of managing tasks and categorizing time spent on various activities, such as UX design and development. Our platform offers beautiful visualizations and a user-friendly experience with minimal clicks required.',
+    heroImageName: 'timeIsPrecious/timeIsPreciousDashboard3.png'
   }
 ]
